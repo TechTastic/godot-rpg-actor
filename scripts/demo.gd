d77ffe41765dbda6956405d8d6999c2f78b05c0e
TechTastic <74630543+TechTastic@users.noreply.github.com>
2026-04-09 17:57:08 -0500

    Docuimentation complete

--- scripts/demo.gd
+++ scripts/demo.gd
@@ -1,19 +1,5 @@
 extends Node
 
 func _ready():
-	var data = await RpgActor.resolve_handle("godotguy.rpg.actor")
-	#print(await RpgActor.metrics())
-	#print(await RpgActor.health())
-	#print(await RpgActor.get_masters_for_player(data.did))
-	#print(await RpgActor.get_masters_by_authority("did:plc:kwgllf365cwmxbnxitx4pjdj"))
-	#print(await RpgActor.get_creator_pricing())
-	#print(await RpgActor.check_creator(data.did))
-	
-	print(await RpgActor.get_record(data.pds, data.did, "equipment.rpg.item", "tops"))
-	#print(await RpgActor._http_request("https://rpg.actor/api/pds-login", HTTPClient.METHOD_POST, [
-		#"Content-Type: application/json"
-	#], JSON.stringify({
-		#"handle": "godotguy.rpg.actor",
-		#"password": "Sep!10Az"
-	#})))
-	pass
+	print(await RpgActor.metrics())
+	print(await RpgActor.health())

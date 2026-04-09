874ee0b0bca380ca6984a7d1b6162f8f1b0f85e0
TechTastic <74630543+TechTastic@users.noreply.github.com>
2026-04-08 20:53:51 -0500

    Almost done, still wanting to look into OAuth flow for AT protocol, but might need ot make a C# plugin to support the encryption standard used

--- addons/rpg_actor/autoload/rpg_actor_api.gd
+++ addons/rpg_actor/autoload/rpg_actor_api.gd
@@ -10,7 +10,7 @@ var blsk_api: String:
 	get: return ProjectSettings.get_setting("rpg_actor/bluesky_api", "https://public.api.bsky.app")
 
 
-## rpg.actor API Handlers
+## Registry
 func get_actors(full: bool = false) -> Dictionary:
 	var endpoint = "actors"
 	if full:
@@ -34,6 +34,7 @@ func health() -> Dictionary:
 	return await _http_request("%s/%s" % [base_api, "health"])
 
 
+## Masters
 func get_masters_for_player(player_did: String) -> Dictionary:
 	return await _http_request("%s/%s%s" % [base_api, "masters?player=", player_did])
 
@@ -42,6 +43,7 @@ func get_masters_by_authority(authority_did: String) -> Dictionary:
 	return await _http_request("%s/%s%s" % [base_api, "masters/by-authority?authority=", authority_did])
 
 
+## Sprites and Media
 func get_sprite(did: String) -> Dictionary:
 	return await _http_request("%s/%s%s" % [base_api, "sprite/normalized?did=", did])
 
@@ -50,6 +52,7 @@ func get_open_graphic_card(did: String) -> Dictionary:
 	return await _http_request("%s/%s%s" % [base_api, "og/image?id=", did])
 
 
+## Equipment
 # TODO: Implement this
 func give_equipment(pds: String, access_token: String, dpop_header: String):
 	pass
@@ -60,6 +63,7 @@ func revoke_equipment():
 	pass
 
 
+## Creator
 func get_creator_pricing() -> Dictionary:
 	return await _http_request("%s/%s" % [base_api, "creator/pricing"])
 
@@ -67,6 +71,57 @@ func get_creator_pricing() -> Dictionary:
 func check_creator(did: String) -> bool:
 	var data = await _http_request("%s/%s%s" % [base_api, "creator/check?did=", did])
 	return data.get("isCreator", false)
+
+
+## Account
+func pds_login(handle: String, password: String) -> Dictionary:
+	return await _http_request("%s/%s" % [base_api, "api/pds-login"], HTTPClient.METHOD_POST, [
+		"Content-Type: application/json"
+	], JSON.stringify({
+		"handle": handle,
+		"password": password
+	}))
+
+
+func update_email(did: String, password: String, email: String) -> Dictionary:
+	return await _http_request("%s/%s" % [base_api, "api/account/update-email"], HTTPClient.METHOD_POST, [
+		"Content-Type: application/json"
+	], JSON.stringify({
+		"did": did,
+		"password": password,
+		"email": email
+	}))
+
+
+func change_password(did: String, current_password: String, new_password: String) -> Dictionary:
+	return await _http_request("%s/%s" % [base_api, "api/account/change-password"], HTTPClient.METHOD_POST, [
+		"Content-Type: application/json"
+	], JSON.stringify({
+		"did": did,
+		"current_password": current_password,
+		"new_password": new_password
+	}))
+
+
+func refresh_actor(did: String) -> Dictionary:
+	return await _http_request("%s/%s" % [base_api, "api/refresh-actor"], HTTPClient.METHOD_POST, [
+		"Content-Type: application/json"
+	], JSON.stringify({
+		"did": did
+	}))
+
+
+func contact(email: String, subject: String, message: String, handle: String = "") -> Dictionary:
+	var data = {
+		"email": email,
+		"subject": subject,
+		"message": message
+	}
+	if !handle.is_empty():
+		data.set("handle", handle)
+	return await _http_request("%s/%s" % [base_api, "api/contact"], HTTPClient.METHOD_POST, [
+		"Content-Type: application/json"
+	], JSON.stringify(data))
 
 
 ## XRPC Handlers
@@ -104,10 +159,16 @@ func _http_request(url: String, method: HTTPClient.Method = HTTPClient.METHOD_GE
 	var result = await req.request_completed
 	req.queue_free()
 	var code: int = result[1]
+	var res = JSON.parse_string(result[3].get_string_from_utf8())
 	if code != 200:
-		push_warning("RpgActorXRPC: %s %s returned HTTP Response Code %d" % [ClassDB.class_get_enum_constants("HTTPClient", "Method")[method], url, code])
+		var warning = "XRPC/HTTP: %s %s returned HTTP Response Code %d" % [ClassDB.class_get_enum_constants("HTTPClient", "Method")[method], url, code]
+		if res is Dictionary:
+			var error = res.get("error", "")
+			if !error.is_empty():
+				warning += " with error message: %s" % [error]
+		push_warning(warning)
 		return {}
-	return JSON.parse_string(result[3].get_string_from_utf8())
+	return res
 
 
 func _extract_pds(plc_doc: Dictionary) -> String:
